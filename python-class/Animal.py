715dd0630e29cb3fa6602e862d9f44f4970073e4
zhuyaoqi <13795318537@163.com>
2020-12-04 00:32:30 +0800

    yaml提交数据

--- python-class/Animal.py
+++ python-class/Animal.py
@@ -1,3 +1,6 @@
+import yaml
+
+
 class Animal:
     def __init__(self, name, color, age, gender):
         self.name = name
@@ -15,7 +18,7 @@ class Animal:
 class Cat(Animal):
     def __init__(self, name, color, age, gender, hair):
         super().__init__(name, color, age, gender)
-        self.hair = '短毛'
+        self.hair = hair
 
     def catch(self):
         print(f'{self.name}抓老鼠')
@@ -27,7 +30,7 @@ class Cat(Animal):
 class Dog(Animal):
     def __init__(self, name, color, age, gender, hair):
         super().__init__(name, color, age, gender)
-        self.hair = '长毛'
+        self.hair = hair
 
     def see_home(self):
         print(f'{self.name}看家')
@@ -40,10 +43,29 @@ class Dog(Animal):
 
 
 if __name__ == '__main__':
-    Tom = Cat('Tom', 'yollow', 10, '男', 'black')
-    Tom.catch()
-    Tom.introduce()
-    wangcai = Dog('旺财', 'red', 5, '女', 'null')
-    wangcai.see_home()
-    wangcai.bark()
-    wangcai.introduce()
+    with open('yaml_demo.yml', encoding="UTF-8") as f:
+        dates = yaml.safe_load(f)
+        print(dates)
+
+    name = dates['default']['name']
+    color = dates['default']['color']
+    age = dates['default']['age']
+    gender = dates['default']['gender']
+    hair = dates['default']['hair']
+    if dates['default'] == 'cat':
+        cat1 = Cat(name, color, age, gender, hair)
+        cat1.catch()
+        cat1.introduce()
+    else:
+        dog1 = Dog(name, color, age, gender, hair)
+        dog1.see_home()
+        dog1.bark()
+        dog1.introduce()
+
+    # Tom = Cat('Tom', 'yollow', 10, '男', 'black')
+    # Tom.catch()
+    # Tom.introduce()
+    # wangcai = Dog('旺财', 'red', 5, '女', 'null')
+    # wangcai.see_home()
+    # wangcai.bark()
+    # wangcai.introduce()
