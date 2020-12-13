79a5e8c5f15f67c04e073d16fac55c6e4d924a42
zhuyaoqi <13795318537@163.com>
2020-12-13 15:32:24 +0800

    完成测试作业3

--- test_fixtrue/conftest.py
+++ test_fixtrue/conftest.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pytest
 import yaml
 import os
@@ -30,3 +32,18 @@ def get_add_dates(request):
 def get_div_dates(request):
     date = request.param
     return date
+
+
+def pytest_collection_modifyitems(
+    session: "Session", config: "Config", items: List["Item"]
+) -> None:
+    """Called after collection has been performed. May filter or re-order
+    the items in-place.
+
+    :param pytest.Session session: The pytest session object.
+    :param _pytest.config.Config config: The pytest config object.
+    :param List[pytest.Item] items: List of item objects.
+    """
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item._nodeid.encode('utf-8').decode('unicode-escape')
