a85f8f13b6b067a4e61daea6de0bcb2353189024
13795318537 <932571861@qq.com>
2020-12-25 17:23:22 +0800

    selenium_test2.5

--- sele_test/selenium_po/Test_po/test_po/test_case1.py
+++ sele_test/selenium_po/Test_po/test_po/test_case1.py
@@ -1,3 +1,5 @@
+import pytest
+
 from sele_test.selenium_po.page.main_page import MainPage
 
 
@@ -9,6 +11,10 @@ class TestLogin:
     def teardown(self):
         pass
 
-    def test_login(self):
-        namelist = self.main.goto_contact_page().click_add_member().add_member().get_member()
-        assert "7111235" in namelist
+    @pytest.mark.parametrize("name, id, phone", [('ddd1', 'id111', '13100001111')])
+    def test_login(self, name, id, phone):
+        # name = '5544'
+        # id = 'wreterte'
+        # phone = '13299991111'
+        namelist = self.main.goto_contact_page().click_add_member().add_member(name, id, phone).get_member()
+        assert name in namelist
--- sele_test/selenium_po/page/add_member_page.py
+++ sele_test/selenium_po/page/add_member_page.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from sele_test.selenium_po.page.Base_page import BasePage
 from sele_test.selenium_po.page.contact_page import ContactPage
@@ -6,18 +7,22 @@ from sele_test.selenium_po.page.contact_page import ContactPage
 
 class AddMemberPage(BasePage):
 
-    def add_member(self):
-        self.driver.find_element_by_id('username').send_keys('7111235')
-        self.driver.find_element_by_id('memberAdd_acctid').send_keys('739231003')
+    def add_member(self, name, id, phone):
+        namea = (By.ID, "username")
+        ida = (By.ID, "memberAdd_acctid")
+        phonea = (By.ID, "memberAdd_phone")
+
+        self.driver.find_element(*namea).send_keys(name)
+        self.driver.find_element(*ida).send_keys(id)
         self.driver.find_element_by_xpath('//input[@type="radio" and @value="2"]').click()
-        self.driver.find_element_by_id('memberAdd_phone').send_keys('13711112222')
+        self.driver.find_element(*phonea).send_keys(phone)
         self.driver.find_element_by_link_text('保存').click()
         return ContactPage(self.driver)
 
-    def add_member_fail(self):
-        self.driver.find_element_by_id('username').send_keys('117111235')
-        self.driver.find_element_by_id('memberAdd_acctid').send_keys('11739231003')
-        self.driver.find_element_by_xpath('//input[@type="radio" and @value="2"]').click()
-        self.driver.find_element_by_id('memberAdd_phone').send_keys('19111112222')
-        self.driver.find_element_by_link_text('保存').click()
-        return ContactPage(self.driver)
+    # def add_member_fail(self):
+    #     self.driver.find_element_by_id('username').send_keys('117111235')
+    #     self.driver.find_element_by_id('memberAdd_acctid').send_keys('11739231003')
+    #     self.driver.find_element_by_xpath('//input[@type="radio" and @value="2"]').click()
+    #     self.driver.find_element_by_id('memberAdd_phone').send_keys('19111112222')
+    #     self.driver.find_element_by_link_text('保存').click()
+    #     return ContactPage(self.driver)
