a417eb71f5e5696f729f646f871dbb207dd6632b
13795318537 <932571861@qq.com>
2021-02-03 14:13:54 +0800

    requests_test learn_apio0203

--- requests_learn/requests2/Base_api.py
+++ requests_learn/requests2/Base_api.py
@@ -4,10 +4,24 @@ from typing import List
 
 
 class Base_Api():
+
+
     def send(self, date):
         return requests.request(**date)
 
     def jsonpath_res(self, ojb, expr):
         return jsonpath(ojb, expr)
 
+    def pytest_collection_modifyitems(
+        session: "Session", config: "Config", items: List["Item"]
+    ) -> None:
+        """Called after collection has been performed. May filter or re-order
+        the items in-place.
 
+        :param pytest.Session session: The pytest session object.
+        :param _pytest.config.Config config: The pytest config object.
+        :param List[pytest.Item] items: List of item objects.
+        """
+        for item in items:
+            item.name = item.name.encode('utf-8').decode('unicode-escape')
+            item._nodeid = item._nodeid.encode('utf-8').decode('unicode-escape')
--- requests_learn/requests2/test_titlecase.py
+++ requests_learn/requests2/test_titlecase.py
@@ -3,22 +3,6 @@ import os
 import pytest
 import yaml
 from requests_learn.requests2.title import Title_Api
-from typing import List
-
-def pytest_collection_modifyitems(
-    session: "Session", config: "Config", items: List["Item"]
-) -> None:
-    """Called after collection has been performed. May filter or re-order
-    the items in-place.
-
-    :param pytest.Session session: The pytest session object.
-    :param _pytest.config.Config config: The pytest config object.
-    :param List[pytest.Item] items: List of item objects.
-    """
-    for item in items:
-        item.name = item.name.encode('utf-8').decode('unicode-escape')
-        item._nodeid = item._nodeid.encode('utf-8').decode('unicode-escape')
-
 
 
 yaml_file_path = os.path.dirname(__file__) + "/conf.yml"
@@ -48,8 +32,10 @@ class Test_title():
         # assert r1['taglist'][-1]['tagid'] == 49
         ass_tagid = self.title.jsonpath_res(r1, f"$..taglist[?(@.tagid== {as_tagid})].tagid")[0]
         print(ass_tagid)
-        assert ass_tagid == as_tagid
-        assert r['errcode'] == 0
+        if r['errcode'] == 0:
+            assert ass_tagid == as_tagid
+        else:
+            r['errcode'] == 40068
 
     @pytest.mark.parametrize(("tagname", "tagid"), updata_data, ids=updata_myid)
     def test_case_update(self, tagname, tagid):
